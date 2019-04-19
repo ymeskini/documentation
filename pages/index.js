4898d38bbfab2908289c90017716b617dc0d2153
Youssef Meskini <youssef@smartsailors.net>
2019-04-19 13:07:33 +0200

    Setup backend with mongoose

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,34 @@
 import Head from 'next/head';
+import PropTypes from 'prop-types';
+
 import Button from '@material-ui/core/Button';
 import withLayout from '../lib/withLayout';
 
-const Index = () => (
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>Index page</title>
       <meta name="description" content="This is the description of the Index page" />
     </Head>
     <p>Content on Index page</p>
+    <p>
+      Email:
+      {user.email}
+    </p>
     <Button variant="contained">MUI button</Button>
   </div>
 );
+
+Index.getInitialProps = async ({ query }) => ({ user: query.user });
+
+Index.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+Index.defaultProps = {
+  user: null,
+};
 
 export default withLayout(Index);
